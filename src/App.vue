18a816fe278e1da7547936999678fbad4ca3e2c9
Josh Mann <jmann5443@gmail.com>
2017-12-28 14:32:53 -0600

    initial commit of scaffolded front-end using vue, vuetify, using vue-webpack proj template from vue-cli.

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<script>
+
+  import VBadge from 'vuetify/es5/components/VBadge/VBadge';
+
+  export default {
+    components: { VBadge },
+    name: 'app',
+    data: () => ({
+      drawer: null,
+    }),
+    props: {
+      source: String,
+    },
+  };
+</script>
+
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      fixed
+      v-model="drawer"
+      app
+    >
+      <v-badge></v-badge>
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Test</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout
+          justify-center
+          align-center
+        >
+          <router-view></router-view>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
